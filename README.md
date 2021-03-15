038a012784563cf7d197edcf54975a7139025fc8
MihirSharma64 <mihirsharma64@gmail.com>
2021-03-15 13:53:58 +0530

    Initial commmit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cricbuzz-Scrapper
+Cricbuzz Scrapper using Selenium Webdriver and JavaScript
+
+Automatically stores the batting and bowling stats of all the players and also their career stats from the matchID provided by the user with the help of Selenium Webdriver.
---
+++ script.js
@@ -0,0 +1,61 @@
+require('chromedriver'); //variable mei isliye nhi liya kyunki hume koi zarurat nhi
+
+let wd = require('selenium-webdriver');
+let matchid = 35502;
+let browser = new wd.Builder().forBrowser('chrome').build(); // control the browser
+
+let batsmankeys = ['playerName', 'outType', 'runs', 'balls', 'fours', 'sixes', 'strikeRate'];
+let bowlerkeys = ['playerName', 'overs', 'maidens', 'runs', 'wickets', 'noball','wide','economy'];
+
+let batsmanData = [];
+let bowlerData = [];
+
+let innings = 1;
+
+async function main(){
+	await browser.get(`https://www.cricbuzz.com/live-cricket-scores/${matchid}`); //URL
+
+	await browser.wait(wd.until.elementLocated(wd.By.css('.cb-nav-bar a')));
+
+	//////////////////////////////// Click the scorecard button///////////////////////
+	let navbarbuttons = await browser.findElements(wd.By.css('.cb-nav-bar a'));
+	await navbarbuttons[1].click();
+	//////////////////////////////////////////////////////////////////////////////////
+
+	await browser.wait(wd.until.elementLocated(wd.By.css(`#innings_${innings} .cb-col.cb-col-100.cb-ltst-wgt-hdr`))); //wait jab tak page load nhi hota
+	let alltables = await browser.findElements(wd.By.css(`#innings_${innings} .cb-col.cb-col-100.cb-ltst-wgt-hdr`));
+
+	let batsmanRow = await alltables[0].findElements(wd.By.css('.cb-col.cb-col-100.cb-scrd-itms'));
+
+	let length = batsmanRow.length;
+	for (let i = 0; i < length - 3; i++) {
+		let batsmanCol = await batsmanRow[i].findElements(wd.By.css('div'));
+		let data = {};
+		for (j in batsmanCol) {
+			if (j != 1) {
+				data[batsmankeys[j]] = await batsmanCol[j].getAttribute('innerText');
+			}
+		}
+		batsmanData.push(data);
+	}
+
+
+
+	let bowlerrows = await alltables[1].findElements(wd.By.css('.cb-col.cb-col-100.cb-scrd-itms'));
+
+	for(i in bowlerrows){
+
+		let bowlercols = await bowlerrows[i].findElements(wd.By.css('div'));
+		let data = {};
+		for(j in bowlercols){
+			data[bowlerkeys[j]] =  await bowlercols[j].getAttribute('innerText');
+		}
+		bowlerData.push(data);
+	}
+
+	console.log(batsmanData);
+	console.log(bowlerData);
+}
+
+
+main();
